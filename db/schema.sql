b5bc3f2bf9058c6f25d4e4d5e22cdf9b136f0150
Blake Reece <frankiereece3@gmail.com>
2023-05-23 16:49:49 -0700

    moved insert to seeds. Working on server.js

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
---   PRIMARY KEY (id),
---   FOREIGN KEY (department_id) REFERENCES department(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,27 +24,11 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
---   PRIMARY KEY (id),
---   FOREIGN KEY (role_id) REFERENCES role(id),
---   FOREIGN KEY (manager_id) REFERENCES employee(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-INSERT INTO department (name) VALUES
-  ('Sales'),
-  ('Research'),
-  ('Finance'),
-  ('Marketing');
 
-INSERT INTO role (id, salary, title, department_id) VALUES
-  (1, 100000, 'Sales Lead', 1),
-  (2, 80000, 'Research Manager', 1),
-  (3, 90000, 'Finance Director', 2),
-  (4, 60000, 'Marketing Lead', 2);
-
-  INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-  (1, 'John', 'Doe', 1, NULL),
-  (2, 'Jim', 'Chance', 2, 1),
-  (3, 'Anne', 'Smith', 3, 1),
-  (4, 'Tim', 'Black', 4, 1);
   
  
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,4 +12,9 @@ INSERT INTO role (id, salary, title, department_id) VALUES
   (3, 90000, 'Finance Director', 2),
   (4, 60000, 'Marketing Lead', 2);
 
-  
+    INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
+    (1, 'John', 'Doe', 1, NULL),
+    (2, 'Jim', 'Chance', 2, 1),
+    (3, 'Anne', 'Smith', 3, 1),
+    (4, 'Tim', 'Black', 4, 1);
+    
