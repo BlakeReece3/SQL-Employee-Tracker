cdd7a7dafc90d4a42a25912430133708240d306d
Blake Reece <frankiereece3@gmail.com>
2023-05-30 11:13:25 -0700

    added queestions array to server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { async } = require('jshint/src/prod-params');
 require('dotenv').config();
 
 
 // Connect to database
 
-const db = mysql.createConnection(
+const database = mysql.createConnection(
     {
         host: 'localhost',
         user: process.env.DB_USER,
@@ -15,3 +16,47 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
 );
+
+const questions = [
+    {
+      name: 'selection',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        {
+          name: 'View all departments',
+          value: 'viewDepartments',
+        },
+        {
+          name: 'View all roles',
+          value: 'viewRoles',
+        },
+        {
+          name: 'View all employees',
+          value: 'viewEmployees',
+        },
+        {
+          name: 'Add a department',
+          value: 'addDepartment',
+        },
+        {
+          name: 'Add a role',
+          value: 'addRole',
+        },
+        {
+          name: 'Add an employee',
+          value: 'addEmployee',
+        },
+        {
+          name: 'Update an employee role',
+          value: 'updateEmployeeRole',
+        },
+        {
+          name: 'Exit',
+          value: 'exit',
+        },
+      ],
+    },
+  ];
+  
+
